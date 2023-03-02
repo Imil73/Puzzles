180d1ff71e41565a1fa701b7e3e5d996c475a3ee
Imil <Mil2163444@mail.ru>
2023-03-02 17:52:13 +0300

    16. Дано число обозначающее день недели. // Выяснить является номер дня недели выходным

---
+++ Lesson_16/Program.cs
@@ -0,0 +1,20 @@
+﻿//16. Дано число обозначающее день недели.
+// Выяснить является номер дня недели выходным
+System.Console.WriteLine( "Введите день недели который вы хотите проверить: " );
+int number = int.Parse( Console.ReadLine() );
+
+if(number == 1 ||number == 2 || number == 3 || number == 4 || number == 5)
+{
+    System.Console.WriteLine( $"Этот день рабочий." );
+}
+else
+{
+    if(number == 6 || number ==7 )
+    {
+        System.Console.WriteLine( $"Этот день выходной." );
+    }
+    else
+    {
+        System.Console.WriteLine( $"Error" );
+    }
+}
