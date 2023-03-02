5a5100d2f114cfd9a9d1d6f1c6ff0dc7733319bd
Imil <Mil2163444@mail.ru>
2023-03-03 00:21:31 +0300

    //Программа проверяет пятизначное число на палиндромом.

---
+++ Lesson_21/Program.cs
@@ -0,0 +1,15 @@
+﻿ //Программа проверяет пятизначное число на палиндромом.
+ System.Console.WriteLine( "Введите число: " );
+int number = int.Parse( Console.ReadLine() );
+int[] array = new int[5];
+
+for(int i = 0; i < 5; i ++)
+{
+array[i] = number %10;
+number = number / 10;               
+}
+
+if(array[0] == array[4] && array[1] == array[3])
+ {Console.WriteLine($"Число полиндром");  }
+else
+{Console.WriteLine($"Число не полиндром");  }
