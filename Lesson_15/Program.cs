515ed8058e82c58fcec2dbfc636241fa782a1390
Imil <Mil2163444@mail.ru>
2023-03-02 16:10:35 +0300

    Дано число. Проверить кратно ли оно 7 и 23

---
+++ Lesson_15/Program.cs
@@ -0,0 +1,12 @@
+﻿//15. Дано число. Проверить кратно ли оно 7 и 23
+System.Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+if(number %7 == 0 || number %23 == 0) 
+{
+    System.Console.WriteLine($"Число кратно");
+}
+else
+{
+   System.Console.WriteLine($"Число некратно");
+} 
