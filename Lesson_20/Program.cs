b1050d1de06100aa2ccaa5e94fc3ca1b6c1242f7
Imil <Mil2163444@mail.ru>
2023-03-02 23:22:21 +0300

    /20. Задать номер четверти, показать диапазоны для возможных координат

---
+++ Lesson_20/Program.cs
@@ -0,0 +1,12 @@
+﻿//20. Задать номер четверти, показать диапазоны для возможных координат
+System.Console.WriteLine( "Введите номер четверти: " );
+int quarter = int.Parse( Console.ReadLine() );
+
+if(quarter == 1) Console.WriteLine($"X (0 < бесконечность) Y (0 < бесконечность)");
+if(quarter == 2) Console.WriteLine($"X (0 > бесконечность) Y (0 < бесконечность)");
+if(quarter == 3) Console.WriteLine($"X (0 > бесконечность) Y (0 > бесконечность)");
+if(quarter == 4) Console.WriteLine($"X (0 < бесконечность) Y (0 > бесконечность)");
+
+
+
+
