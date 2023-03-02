516caeb80cae0b0f822ab705dec204c099b7025b
Imil <Mil2163444@mail.ru>
2023-03-02 22:07:15 +0300

    Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y

---
+++ Lesson_18/Program.cs
@@ -0,0 +1,13 @@
+﻿//18. Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+int numberX = 5;
+int numberY = 2;
+int revers = -1;
+
+if (revers * (numberX + numberY) == revers * numberX * revers * numberY )
+{
+    System.Console.WriteLine($"Истинно");
+}
+else
+{
+    System.Console.WriteLine($"Неистинно");
+}
