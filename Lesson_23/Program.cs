41a697761773d6fa4f1525119d9aa658e18fe8f2
Imil <Mil2163444@mail.ru>
2023-03-06 12:55:00 +0300

    Показать таблицу квадратов чисел от 1 до N

---
+++ Lesson_23/Program.cs
@@ -0,0 +1,20 @@
+﻿//3. Показать таблицу квадратов чисел от 1 до N 
+System.Console.WriteLine("Введите число: ");
+int number = int.Parse(System.Console.ReadLine());
+int[] array = new int[number];
+int quatro = 0;
+int cifr = 1;
+
+for (int i = 0; i < number; i++)
+{
+array[i] = cifr ; cifr += 1;
+
+}
+for (int i = 0; i < array.Length; i++)
+{  
+Console.WriteLine($"{array[i]} * {array[i]} = {array[i] * array[i]}");
+}
+
+
+
+
